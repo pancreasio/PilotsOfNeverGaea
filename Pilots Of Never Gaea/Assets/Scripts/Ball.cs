3c0b0d609196e43a474950d60dfc4fe5e4f5b2f6
pancreasio <julianholisu@gmail.com>
2019-08-19 21:35:47 -0300

    functional pong

---
+++ Pilots Of Never Gaea/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    private float bounceTime = 0.1f, bounceClock;
+    public float initialSpeed, bound;
+    void Start()
+    {
+        bounceClock = 0;
+        rig.AddForce((-transform.up + transform.right) * initialSpeed, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        bounceClock += Time.deltaTime;
+        if (transform.position.x >= bound || transform.position.x <= -bound)
+        {
+            if (bounceClock > bounceTime)
+            {
+                Debug.Log(rig.velocity);
+                rig.velocity = new Vector2(-rig.velocity.x, rig.velocity.y);
+                bounceClock = 0;
+            }
+        }
+    }
+}
