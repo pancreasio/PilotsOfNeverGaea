5f0b94b247cf1cf1ac76f4f59ff760081db33d19
pancreasio <julianholisu@gmail.com>
2019-10-09 09:49:17 -0300

    platforms implemented
    also deleted remaining Library and Obj files

--- Pilots Of Never Gaea/Assets/Scripts/Managers/LevelManager.cs
+++ Pilots Of Never Gaea/Assets/Scripts/Managers/LevelManager.cs
@@ -8,13 +8,19 @@ public class LevelManager : MonoBehaviour
 {
     private int p1Score, p2Score;
     public TextMeshProUGUI p1ScoreText, p2ScoreText, p1WinText, p2WinText;
-    public GameObject gameOverUI;
+    public GameObject gameOverUI, leftPlatform, rightPlatform;
     public static GameManager.ButtonAction RetryAction, ExitAction;
+    public float platformDelay, platformSpeed, platformLimit;
+    private float platformClock;
+    private bool platformsClosing, platformsArrived;
 
     private void Start()
     {
         p1Score = 0;
         p2Score = 0;
+        platformClock = 0;
+        platformsClosing = false;
+        platformsArrived = false;
         Ball.onScore = PlayerScored;
         Time.timeScale = 1;
     }
@@ -33,6 +39,23 @@ public class LevelManager : MonoBehaviour
 
     private void Update()
     {
+        platformClock += Time.deltaTime;
+        if (platformsClosing)
+        {
+            leftPlatform.transform.Translate(transform.right * platformSpeed * Time.deltaTime);
+            rightPlatform.transform.Translate(-transform.right * platformSpeed * Time.deltaTime);
+            if (leftPlatform.transform.position.x >= platformLimit)
+            {
+                platformsArrived = true;
+                platformsClosing = false;
+            }
+        }
+        else
+        {
+            if (!platformsArrived && platformClock >= platformDelay)
+                platformsClosing = true;
+        }
+
         p1ScoreText.text = p1Score.ToString();
         p2ScoreText.text = p2Score.ToString();
         if (p1Score >= 3)
