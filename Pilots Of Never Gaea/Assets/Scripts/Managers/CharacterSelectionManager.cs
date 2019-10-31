ca2d6ccd1cfab816bdc49cadb886cdbbde5dfbe0
pancreasio <julianholisu@gmail.com>
2019-10-30 23:55:42 -0300

    character selection intro added

--- Pilots Of Never Gaea/Assets/Scripts/Managers/CharacterSelectionManager.cs
+++ Pilots Of Never Gaea/Assets/Scripts/Managers/CharacterSelectionManager.cs
@@ -11,43 +11,73 @@ public class CharacterSelectionManager : MonoBehaviour
     public static GameManager.StartDuelFunction SelectAction;
     public static GameManager.SceneChange FadeAction;
     private Character p1SelectedCharacter, p2SelectedCharacter;
-    public float platformTimeToDissapear, platformLimit;
+    public float platformTime, platformLimit;
     private float platformClock;
-    public GameObject p1SelectedButton, p2SelectedButton, p1Elements, p2Elements;
+    private Vector3 leftPlatformStartingPosition, rightPlatformStartingPosition;
+    public GameObject p1Elements, p2Elements;
+    public AutoScroll p1Scroll, p2Scroll;
     public List<GameObject> shipButtons;
-    private bool opening = false;
+    private bool opening = false, closing = true;
 
     private void Start()
     {
         p1SelectedCharacter = Character.nullCharacter;
         p2SelectedCharacter = Character.nullCharacter;
+        leftPlatformStartingPosition = p1Elements.transform.position;
+        rightPlatformStartingPosition = p2Elements.transform.position;
+        p1Elements.transform.Translate(-transform.right * platformLimit);
+        p2Elements.transform.Translate(transform.right * platformLimit);
+        platformClock = 0f;
+        SetActiveButtons(false);
     }
 
     private void Update()
     {
-        if (opening || (p1SelectedCharacter != Character.nullCharacter && p2SelectedCharacter != Character.nullCharacter))
+        if (closing)
         {
-            if (!opening)
+            if (platformClock < platformTime)
             {
-                DestroyButtons();
-                opening = true;
-                Destroy(Camera.main.gameObject);
-                if (SelectAction != null)
-                    SelectAction(p1SelectedCharacter, p2SelectedCharacter);
+                platformClock += Time.deltaTime;
+                float speed = platformLimit / platformTime * Time.deltaTime;
+                p1Elements.transform.Translate(transform.right * speed);
+                p2Elements.transform.Translate(-transform.right * speed);
             }
             else
             {
-                platformClock += Time.deltaTime;
-                if (platformClock < platformTimeToDissapear)
+                p1Elements.transform.position = leftPlatformStartingPosition;
+                p2Elements.transform.position = rightPlatformStartingPosition;
+                SetActiveButtons(true);
+                closing = false;
+            }
+
+        }
+        else
+        {
+            if (opening || (p1SelectedCharacter != Character.nullCharacter && p2SelectedCharacter != Character.nullCharacter))
+            {
+                if (!opening)
                 {
-                    float speed = platformLimit / platformTimeToDissapear * Time.deltaTime;
-                    p1Elements.transform.Translate(-transform.right * speed);
-                    p2Elements.transform.Translate(transform.right * speed);
+                    platformClock = 0f;
+                    DestroyButtons();
+                    opening = true;
+                    Destroy(Camera.main.gameObject);
+                    if (SelectAction != null)
+                        SelectAction(p1SelectedCharacter, p2SelectedCharacter);
                 }
                 else
                 {
-                    if (FadeAction != null)
-                        FadeAction(1);
+                    platformClock += Time.deltaTime;
+                    if (platformClock < platformTime)
+                    {
+                        float speed = platformLimit / platformTime * Time.deltaTime;
+                        p1Elements.transform.Translate(-transform.right * speed);
+                        p2Elements.transform.Translate(transform.right * speed);
+                    }
+                    else
+                    {
+                        if (FadeAction != null)
+                            FadeAction(1);
+                    }
                 }
             }
         }
@@ -59,6 +89,16 @@ public class CharacterSelectionManager : MonoBehaviour
         {
             Destroy(button);
         }
+    }
+
+    private void SetActiveButtons(bool active)
+    {
+        foreach (GameObject button in shipButtons)
+        {
+            button.SetActive(active);
+        }
+        p1Scroll.enabled = active;
+        p2Scroll.enabled = active;
     }
 
     public void P1SelectCharacter(int character)
