65a52426501e21afacb78216ed4fbd1d90120221
pancreasio <julianholisu@gmail.com>
2019-12-03 21:46:05 -0300

    character selection animations added
    fixed typo in credits and updated old logos and menus

---
+++ Pilots Of Never Gaea/Assets/Scripts/UI/ShipSign.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSign : MonoBehaviour
+{
+    public Animator spriteAnimator;
+
+    public void SetIdle()
+    {
+        spriteAnimator.SetBool("IDLE", true);
+        spriteAnimator.SetBool("HIGHLIGHTED", false);
+    }
+
+    public void SetHighlighted()
+    {
+        spriteAnimator.SetBool("IDLE", false);
+        spriteAnimator.SetBool("HIGHLIGHTED", true);
+    }
+
+    public void SetSelected()
+    {
+
+    }
+}
