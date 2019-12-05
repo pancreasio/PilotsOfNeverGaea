25bbfed99556eed82d33abf2970c063c1e31aacb
pancreasio <julianholisu@gmail.com>
2019-12-04 22:14:15 -0300

    random backgrounds added

---
+++ Pilots Of Never Gaea/Assets/BackgroundRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRandomizer : MonoBehaviour
+{
+    public List<Sprite> backgroundList;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = GetRandomBackground();
+    }
+
+    private Sprite GetRandomBackground()
+    {
+        Sprite background = backgroundList[Random.Range(0, backgroundList.Count)];
+        return background;
+    }
+}
