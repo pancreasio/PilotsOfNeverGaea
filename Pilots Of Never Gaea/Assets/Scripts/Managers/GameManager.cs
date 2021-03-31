5f554a1a521753e15f4485f3d848e38d360e1b28
pancreasio <julianholisu@gmail.com>
2021-03-31 09:23:59 -0300

    Basic input management implemented

--- Pilots Of Never Gaea/Assets/Scripts/Managers/GameManager.cs
+++ Pilots Of Never Gaea/Assets/Scripts/Managers/GameManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -12,6 +15,12 @@ public class GameManager : MonoBehaviour
     public delegate void GameOverFunction(bool value);
     private static GameObject gameManagerInstance;
     private int currentScene;
+
+    [SerializeField]
+    protected GameObject playerPrefab;
+
+    public GameObject player1;
+    public GameObject player2;
 
     private void Awake()
     {
@@ -31,6 +40,10 @@ public class GameManager : MonoBehaviour
     {
         Screen.SetResolution(1920, 1080, true);
         currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        InstantiatePlayer(1);
+        InstantiatePlayer(2);
+
         MenuManager.StartAction = NextScene;
         MenuManager.ExitAction = ExitApplication;
         LevelManager.GameOverAction = GameOver;
@@ -42,6 +55,27 @@ public class GameManager : MonoBehaviour
         GameOverManager.RematchAction = PreviousScene;
         GameOverManager.CharacterSelectAction = LoadScene;
         GameOverManager.ExitAction = LoadScene;
+    }
+
+    private void InstantiatePlayer(int playerNumber)
+    {
+        if (0 < playerNumber && playerNumber < 3)
+        {
+            GameObject newPlayer = Instantiate(playerPrefab);
+            newPlayer.transform.parent = this.transform;
+            newPlayer.name = "Player " + playerNumber.ToString();
+            newPlayer.GetComponent<User>().playerNumber = playerNumber;
+            //InputUser.PerformPairingWithDevice(device: Keyboard.current, newPlayer.GetComponent<PlayerInput>().user);
+            switch (playerNumber)
+            {
+                case 1:
+                    player1 = newPlayer;
+                    break;
+                case 2:
+                    player2 = newPlayer;
+                    break;
+            }
+        }
     }
 
     private void ReloadScene()
---
+++ Pilots Of Never Gaea/Assets/Scripts/Session/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class User : MonoBehaviour
+{
+    public CharacterSelectionManager.Character selectedCharacter;
+    public int playerNumber;
+    public Palette currentPalette;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<PlayerInput>().SwitchCurrentControlScheme("Keyboard", devices: Keyboard.current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetAsPlayer(int newPlayerNumber)
+    {
+        if(0 < newPlayerNumber && playerNumber < 3)
+            playerNumber = newPlayerNumber;
+    }
+}
