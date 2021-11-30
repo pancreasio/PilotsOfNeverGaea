c79fd8b244da3fe23874b8aedc1329af0bb8a289
pancreasio <julianholisu@gmail.com>
2021-11-30 10:20:36 -0300

    Control Selection Menu UI completed

--- Pilots Of Never Gaea/Assets/Scripts/Managers/ControlSelectionManager.cs
+++ Pilots Of Never Gaea/Assets/Scripts/Managers/ControlSelectionManager.cs
@@ -7,11 +7,21 @@ public class ControlSelectionManager : MonoBehaviour
 {
     PlayerInputManager playerInputManager;
 
-    private static GameObject controlSelectionManagerInstance;
+    public static GameObject controlSelectionManagerInstance;
     bool isPlayer1Ready;
     bool isPlayer2Ready;
+
+    public float deviceUIOffset = 1f;
+    public Sprite WASDSprite;
+    public Sprite arrowKeysSprite;
+    public Sprite gamepadSprite;
     public GameObject controllerUIEntity;
-    List<InputDevice> inputDeviceList = new List<InputDevice>();
+    public Transform p1UIPosition;
+    public Transform p2UIPosition;
+    public Transform deviceListUIPosition;
+    List<GameObject> playerInputList = new List<GameObject>();
+    GameObject p1AssignedController;
+    GameObject p2AssignedController;
     // Start is called before the first frame update
 
 
@@ -35,12 +45,6 @@ public class ControlSelectionManager : MonoBehaviour
     }
     void Start()
     {
-
-        //inputDeviceList.AddRange(InputSystem.devices);
-        foreach (InputDevice i in InputSystem.devices)
-        {
-            Debug.Log(i.name);
-        }
         OnActivate();
     }
 
@@ -50,29 +54,102 @@ public class ControlSelectionManager : MonoBehaviour
 
     }
 
+    public void MoveUIController(ControllerDeviceUI controller, bool isMovingLeft)
+    {
+        if (playerInputList.Contains(controller.gameObject))
+        {
+            if (isMovingLeft)
+            {
+                if (p1AssignedController == null)
+                {
+                    AssignPlayerController(controller.gameObject, true);
+                    playerInputList.Remove(controller.gameObject);
+                }
+            }
+
+            else
+                if (p2AssignedController == null)
+            {
+                AssignPlayerController(controller.gameObject, false);
+                playerInputList.Remove(controller.gameObject);
+            }
+        }
+        else
+        if (p1AssignedController == controller.gameObject && !isMovingLeft)
+        {
+            p1AssignedController = null;
+            ReturnControllerToList(controller.gameObject);
+            Debug.Log("should return");
+        }
+        else
+        if (p2AssignedController == controller.gameObject && isMovingLeft)
+        {
+            p2AssignedController = null;
+            ReturnControllerToList(controller.gameObject);
+            Debug.Log("should return");
+        }
+    }
+
     public void OnActivate()
     {
         int i = 0;
         PlayerInput p1Input = PlayerInput.Instantiate(controllerUIEntity, i, "Keyboard", -1, Keyboard.current.device);
         p1Input.transform.SetParent(this.transform);
-        p1Input.transform.Translate(-p1Input.transform.up * i * 2);
-
+        p1Input.transform.SetPositionAndRotation(deviceListUIPosition.position - (Vector3.up * i * deviceUIOffset),Quaternion.identity);
+        p1Input.transform.GetComponent<SpriteRenderer>().sprite = WASDSprite;
+        p1Input.transform.GetComponent<ControllerDeviceUI>().assignedDevice = Keyboard.current.device;
+        p1Input.transform.GetComponent<ControllerDeviceUI>().assignedScheme = p1Input.currentControlScheme;
 
         i++;
         PlayerInput p2Input = PlayerInput.Instantiate(controllerUIEntity, i, "Keyboard Player 2", -1, Keyboard.current.device);
         p2Input.transform.SetParent(this.transform);
-        p2Input.transform.Translate(-p2Input.transform.up * i * 2);
+        p2Input.transform.SetPositionAndRotation(deviceListUIPosition.position - (Vector3.up * i * deviceUIOffset),Quaternion.identity);
+        p2Input.transform.GetComponent<SpriteRenderer>().sprite = arrowKeysSprite;
+        p2Input.transform.GetComponent<ControllerDeviceUI>().assignedDevice = Keyboard.current.device;
+        p2Input.transform.GetComponent<ControllerDeviceUI>().assignedScheme = p2Input.currentControlScheme;
+
+        playerInputList.Add(p1Input.gameObject);
+        playerInputList.Add(p2Input.gameObject);
+
 
         for (int g = 0; g < Gamepad.all.Count; g++)
         {
             i++;
             PlayerInput gamepadInput = PlayerInput.Instantiate(controllerUIEntity, i, "Gamepad", -1, Gamepad.all[g].device);
-            Debug.Log(Gamepad.all[g].device.name);
             gamepadInput.transform.SetParent(this.transform);
-            gamepadInput.transform.Translate(-gamepadInput.transform.up * i * 2);
+            gamepadInput.transform.SetPositionAndRotation(deviceListUIPosition.position - (Vector3.up * i * deviceUIOffset),Quaternion.identity);
+            gamepadInput.transform.GetComponent<SpriteRenderer>().sprite = gamepadSprite;
+            gamepadInput.transform.GetComponent<ControllerDeviceUI>().assignedDevice = Gamepad.all[g].device;
+            gamepadInput.transform.GetComponent<ControllerDeviceUI>().assignedScheme = "Gamepad";
+            playerInputList.Add(gamepadInput.gameObject);
         }
     }
 
+
+    private void AssignPlayerController(GameObject UIController, bool isPlayer1)
+    {
+        if (isPlayer1)
+        {
+            UIController.transform.SetPositionAndRotation(p1UIPosition.position, Quaternion.identity);
+            p1AssignedController = UIController;
+        }
+        else
+        {
+            UIController.transform.SetPositionAndRotation(p2UIPosition.position, Quaternion.identity);
+            p2AssignedController = UIController;
+        }
+    }
+
+    private void ReturnControllerToList(GameObject UIController)
+    {
+        playerInputList.Add(UIController);
+        int i=0;
+        foreach (GameObject controllerObject in playerInputList)
+        {
+            controllerObject.transform.SetPositionAndRotation(deviceListUIPosition.transform.position - Vector3.up * deviceUIOffset * i, Quaternion.identity);
+            i++;
+        }
+    }
     public void OnDeviceChange(InputDevice inputDevice, InputDeviceChange inputDeviceChange)
     {
         switch (inputDeviceChange)
--- Pilots Of Never Gaea/Assets/Scripts/UI/ControllerDeviceUI.cs
+++ Pilots Of Never Gaea/Assets/Scripts/UI/ControllerDeviceUI.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControllerDeviceUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputDevice assignedDevice = null;
+    public string assignedScheme;
+    private bool canMove = true;
+    public void MoveToPlayer(InputAction.CallbackContext context)
+    {
+        Vector2 inputValue = context.ReadValue<Vector2>();
+        if(inputValue.x>0.5f && canMove)
+            TryMoving(false);
+            else
+            if(inputValue.x<-0.5f && canMove)
+            TryMoving(true);
+
+        if(inputValue == Vector2.zero)
+            canMove=true;
+    }
+
+    private void TryMoving(bool isMovingLeft)
+    {
+        ControlSelectionManager.controlSelectionManagerInstance.GetComponent<ControlSelectionManager>().MoveUIController(this, isMovingLeft);
+        canMove = false;
+    }
+
+    public void TryConfirming()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TryCancelling()
     {
-        
-    }
 
-    public void MoveToPlayer()
-    {
-        transform.Translate(transform.right);
     }
 }
